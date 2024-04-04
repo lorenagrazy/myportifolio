e1abf8f54643d34675e64b02f2798cf487ea12d2
Lorena Silva <lorenagrazy@yahoo.com.br>
2024-04-04 16:28:48 -0300

    Update Contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,45 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  FormControl,
-} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css'],
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
   contactForm!: FormGroup;
   isError = false;
   errorMessage = '';
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private router: Router,
-    private http: HttpClient
-  ) {}
+  constructor(private formBuilder: FormBuilder, private router: Router, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.contactForm = this.formBuilder.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      phone: ['', [Validators.required, this.phoneValidator]], // Adicionando validação personalizada
-      body: ['', Validators.required],
+      phone: ['', [Validators.required, this.phoneValidator]], // Adding custom validation
+      body: ['', Validators.required]
     });
   }
 
   submitForm() {
     if (this.contactForm.valid) {
-      this.http
-        .post('https://formspree.io/f/xbjnzzvo', this.contactForm.value)
+      this.http.post('https://formspree.io/f/xbjnzzvo', this.contactForm.value)
         .subscribe(
           (response) => {
             this.router.navigate(['/success']);
+
+            setTimeout(() => {
+              this.router.navigate(['/contact']);
+            }, 4000);
           },
           (error) => {
             this.router.navigate(['/error']);
@@ -53,7 +47,7 @@ export class ContactComponent implements OnInit {
   phoneValidator(control: FormControl): { [s: string]: boolean } | null {
     const PHONE_REGEXP = /^[0-9]{10}$/;
     if (!PHONE_REGEXP.test(control.value)) {
-      return { invalidPhone: true };
+      return { 'invalidPhone': true };
     }
     return null;
   }
